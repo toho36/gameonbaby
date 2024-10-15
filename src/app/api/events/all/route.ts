c7f23c48354715bbdb1ea1c15686ca4ac9d9f886
toho36 <toho0036@gmail.com>
2024-10-15 18:28:39 +0200

    fetching data

--- src/app/api/events/all/route.ts
+++ src/app/api/events/all/route.ts
@@ -12,41 +12,30 @@ type LatestEventResponse = {
   price: number;
   from: Date;
   to: Date;
-};
+}[];
 
 export const GET = withErrorHandling(
   async (req: Request): Promise<NextResponse> => {
     const events: Event[] = await prisma.event.findMany({
-      // orderBy: {
-      //   from: "asc",
-      // },
+      orderBy: {
+        from: "asc",
+      },
     });
 
     if (events.length === 0) {
       throw new ApiError(
-        "There is no event available at the moment.",
+        "There are no events available at the moment.",
         404,
         getCode(Modules.EVENT, ErrorCodes.NOT_FOUND),
       );
     }
 
-    // Assuming you want the first event in the sorted list
-    const event = events[0];
-
-    if (!event) {
-      throw new ApiError(
-        "Event data is unavailable.",
-        404,
-        getCode(Modules.EVENT, ErrorCodes.NOT_FOUND),
-      );
-    }
-
-    const response: LatestEventResponse = {
+    const response: LatestEventResponse = events.map((event) => ({
       id: event.id,
       price: event.price,
       from: event.from,
       to: event.to,
-    };
+    }));
 
     return NextResponse.json(response);
   },
